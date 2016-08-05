68ab8344ad1f783fd292e54e238494d4c818ae56
Simon Guindon <simon@nureality.ca>
2016-08-05 06:41:58 -0400

    Checking in missed changed file.

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
+var (
+	removeHost *string
+)
+
 func main() {
 	app := kingpin.New("cluster", "Cluster management application.")
 
@@ -17,16 +21,43 @@ func main() {
 	lsCommand.Datacenter = ls.Flag("datacenter", "Datacenter to list.").String()
 	lsCommand.Host = ls.Flag("host", "Host to list.").String()
 
-	addCommand := &commands.AddCommand{}
+	addCommand := commands.NewAddCommand()
 	add := app.Command("add", "Add a node to the specified cluster and datacenter.").Action(addCommand.Run)
-	addCommand.Cluster = add.Flag("cluster", "Cluster to add the new node to.").String()
-	addCommand.Datacenter = add.Flag("datacenter", "Datacenter to add the new node to.").String()
-	addCommand.Host = add.Flag("host", "Host to associate with the new node.").String()
-	addCommand.IPAddress = add.Flag("ipaddress", "IP address to associate with the new node.").String()
+	addCommand.NoProgress = add.Flag("noprogress", "Suppress and hide the progress bar.").Bool()
+	addContext := commands.AddContext{
+		Cluster:    add.Flag("cluster", "Cluster to add the new node to.").String(),
+		Datacenter: add.Flag("datacenter", "Datacenter to add the new node to.").String(),
+		Host:       add.Flag("host", "Host to associate with the new node.").String(),
+		IPAddress:  add.Flag("ipaddress", "IP address to associate with the new node.").String(),
+	}
+	addCommand.SetContext(&addContext)
 
-	removeCommand := &commands.RemoveCommand{}
+	addBatchCommand := commands.NewAddBatchCommand()
+	addBatch := app.Command("addbatch", "Add a batch of nodes to the specified cluster and datacenter.").Action(addBatchCommand.Run)
+	addBatchCommand.NoProgress = addBatch.Flag("noprogress", "Suppress and hide the progress bar.").Bool()
+	addBatchContext := commands.AddBatchContext{
+		Nodes:      addBatch.Flag("nodes", "Number of nodes to add the cluster and datacenter.").Int(),
+		Cluster:    addBatch.Flag("cluster", "Cluster to add the new node to.").String(),
+		Datacenter: addBatch.Flag("datacenter", "Datacenter to add the new node to.").String(),
+		HostFormat: addBatch.Flag("hostformat", "Host format to associate with the new nodes.").String(),
+		Subnet:     addBatch.Flag("subnet", "Subnet to associate with the new nodes.").String(),
+	}
+	addBatchCommand.SetContext(&addBatchContext)
+
+	removeCommand := commands.NewRemoveCommand()
 	remove := app.Command("remove", "Remove a node from the cluster.").Action(removeCommand.Run)
-	removeCommand.Host = remove.Arg("host", "Host to remove from the cluster.").String()
+	removeCommand.NoProgress = remove.Flag("noprogress", "Suppress and hide the progress bar.").Bool()
+	removeContext := commands.RemoveContext{Host: remove.Arg("host", "Host to remove from the cluster.").String()}
+	removeCommand.SetContext(&removeContext)
+
+	removeBatchCommand := commands.NewRemoveBatchCommand()
+	removeBatch := app.Command("removebatch", "Add a batch of nodes to the specified cluster and datacenter.").Action(removeBatchCommand.Run)
+	removeBatchCommand.NoProgress = removeBatch.Flag("noprogress", "Suppress and hide the progress bar.").Bool()
+	removeBatchContext := commands.RemoveBatchContext{
+		Nodes:      removeBatch.Flag("nodes", "Number of nodes to add the cluster and datacenter.").Int(),
+		HostFormat: removeBatch.Flag("hostformat", "Host format to associate with the new nodes.").String(),
+	}
+	removeBatchCommand.SetContext(&removeBatchContext)
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
 }
